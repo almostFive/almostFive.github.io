1f8944fbe45b956652d80f7b0b10e3b334e84175
avery cronin <a.cronin74@gmail.com>
2021-09-26 20:49:00 -0400

    Testing some snakeeee

---
+++ jsTest.js
@@ -0,0 +1,91 @@
+'use strict';
+
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
+
+let dx,dy,d;
+let snake = [{x: 16, y: 8}];
+let arr = Array.from({length: 32}, () => Array.from({length: 16}, () => 0));
+
+() => {
+  window.addEventListener('resize', resizeCanvas, false);
+  resizeCanvas();
+}
+
+function resizeCanvas() {
+  if (window.innerHeight < (2 * window.innerWidth)) {
+  	d = Math.floor(window.innerHeight/16);
+  } else {
+  	d = Math.floor(window.innerWidth/32);
+  }
+  arr_x = window.innerWidth;
+  arr_y = window.innerHeight;
+
+	canvas.width = arr_x;
+	canvas.height = arr_y;
+
+	draw();
+}
+
+function clear() {
+  ctx.fillStyle = '#EEEEEE';
+  ctx.fillRect(0, 0, arr_x, arr_y);
+}
+
+function move() {
+  /*
+  let head = {snake[0].x + dx, snake[0].y + dy};
+  snake.unshift(head);
+  snake.pop();
+  */
+}
+
+function draw() {
+  ctx.fillStyle = '#EEEEEE';
+  ctx.fillRect(0, 0, arr_x, arr_y);
+  ctx.fillStyle = '#000000';
+  for (let i = 0; i < snake.length; i++) {
+    ctx.fillRect(snake[i].x, snake[i].y - d, snake[i].x, snake[i].y);
+  }
+}
+
+function main () {
+  setTime(function game() => {
+    clear();
+    move();
+    draw();
+  }, 100);
+}
+
+function fruit () {
+  
+}
+
+document.addEventListener('keydown', event => {
+	const charList = 'adsw';
+	// movement keys only
+	const key = event.key.toLowerCase();
+	if (!(charList.includes(key)) && (key !== 'arrowup') && (key !== 'arrowdown') && (key !== 'arrowright') && (key !== 'arrowleft')) { return; }
+
+	if (key === 'arrowleft' || key === 'a') {
+		dx = -1;
+    dy = 0;
+	} else if (key === 'arrowup' || key === 'w') {
+		dy = 1;
+    dx = 0;
+	} else if (key === 'arrowright' || key === 'd') {
+		dx = 1;
+    dy = 0;
+	} else if (key === 'arrowdown' || key === 's') {
+		dy = -1;
+    dx = 0;
+	}
+
+  function x () {
+  	return Math.floor(Math.random() * 32);
+  }
+
+  function y () {
+  	return Math.floor(Math.random() * 16);
+  }
